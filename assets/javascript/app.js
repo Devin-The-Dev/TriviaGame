69f4d2b79761d516d6d69589c845b3c2225bcc6f
Devin Udy <devin.udy@gmail.com>
2018-11-29 07:09:45 -0700

    Updated radio elements with unique ids, created counters for right and wrong answers, stopped timer and ended game when all questions were answered

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,14 +1,51 @@
-//JS = logic
-//jQuery = display
-
-//timer variable
 //correct answer variable
-//max number of answers
 //for loop for questions answered
 
-//Timer
-var timer = 5;
+var timer = 30;
 var intervalId;
+var correct = 0;
+var wrong = 0;
+var answered = 0;
+
+//Wrong Answers
+document.getElementById("choice1").onclick = function() {
+  wrong++;
+  answered++;
+};
+document.getElementById("choice2").onclick = function() {
+  wrong++;
+  answered++;
+};
+document.getElementById("choice5").onclick = function() {
+  wrong++;
+  answered++;
+};
+document.getElementById("choice6").onclick = function() {
+  wrong++;
+  answered++;
+};
+document.getElementById("choice8").onclick = function() {
+  wrong++;
+  answered++;
+};
+document.getElementById("choice9").onclick = function() {
+  wrong++;
+  answered++;
+};
+
+//Correct Answers
+document.getElementById("choice3").onclick = function() {
+  correct++;
+  answered++;
+};
+document.getElementById("choice4").onclick = function() {
+  correct++;
+  answered++;
+};
+document.getElementById("choice7").onclick = function() {
+  correct++;
+  answered++;
+};
 
 function run() {
   clearInterval(intervalId);
@@ -19,10 +56,9 @@ function decrement() {
 
   $("#timer").text(timer);
 
-  if (timer === 0) {
+  if (timer === 0 || answered === 3) {
     stop();
-
-    alert("Time Up! Your score is ");
+    alert("You answered " + correct + " right and " + wrong + " wrong.");
   }
 }
 
@@ -30,6 +66,4 @@ function stop() {
   clearInterval(intervalId);
 }
 
-//Code correct answer increments
-//stop() when user answers all the questions
 run();
